27ee532e0e1f4a21d8b48ed360b939453df16aab
Rebecca Moore <80188565+rmoore2738@users.noreply.github.com>
2025-03-27 16:27:14 -0600

    Update app.py

--- app.py
+++ app.py
@@ -26,3 +26,11 @@ async def suggest_word(request: Request):
 #async def next_options(request: Request):
     #data = await request.json()
     #return get_next_options(data)
+
+
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run_server(debug=True, host="0.0.0.0", port=port)
+
